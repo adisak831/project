d1bd83c16919b4219b9ae89beecec0e26d8b9f16
DSK <80197360+adisak831@users.noreply.github.com>
2022-04-07 02:45:34 +0700

    aaa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+//import TsetView from './components/Test';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Main from './components/Main';
+import { Container } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    
+    <Main ></Main>
+  );
+}
+
+export default App;
